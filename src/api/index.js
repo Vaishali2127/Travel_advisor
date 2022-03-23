bc5a29fd74e02d0c09aabf8a40d2b71d55e9a80e
Vaishali2127 <vaishalimahour8@gmail.com>
2022-03-23 18:30:02 +0530

    add placedetails

--- src/api/index.js
+++ src/api/index.js
@@ -2,22 +2,24 @@ var axios = require("axios").default;
 
 const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng";
 
-const options = {
-  params: {
-    latitude: "12.91285",
-    longitude: "100.87808",
-  },
-  headers: {
-    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-    "X-RapidAPI-Key": "86876c5448msh7a799bc1bbddd3fp1f39fajsne65d3b918fa9",
-  },
-};
-
 export const getPlacesData = async () => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, options);
+    } = await axios.get(URL, {
+      params: {
+        latitude: "12.91285",
+        longitude: "100.87808",
+        // bl_latitude: sw.lat,
+        // tr_latitude: ne.lat,
+        // bl_longitude: sw.lng,
+        // tr_longitude: ne.lng,
+      },
+      headers: {
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        "X-RapidAPI-Key": "86876c5448msh7a799bc1bbddd3fp1f39fajsne65d3b918fa9",
+      },
+    });
     return data;
   } catch (error) {
     console.log(error);
